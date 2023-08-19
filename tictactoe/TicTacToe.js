1635e524301631ac55767e77680bc4bbb05778c6
Angus M <angus@mm2.ca>
2023-08-19 12:41:01 -0700

    trying to get board to show

--- tictactoe/TicTacToe.js
+++ tictactoe/TicTacToe.js
@@ -15,6 +15,25 @@ class Square extends Component {
     }
 }
 
+class Board extends Component {
+    renderSquare(row, col) {
+        return createElement(Square);
+    }
+    
+    render() {
+        const boardRows = [];
+        for (let i = 0; i < 3; i++) {
+            const row = [];
+            for (let j = 0; j < 3; j++) {
+                row.push(this.renderSquare(i, j));
+            }
+            boardRows.push(createElement('div', { className: 'board-row' }, row));
+        }
+        
+        return createElement('div', null, boardRows);
+    }
+}
+
 const domContainer = document.querySelector('#React-game');
 const root = ReactDOM.createRoot(domContainer);
-root.render(createElement(Square));
+root.render(createElement(Board));
