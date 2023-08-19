13d176a6ad900e45bb8c281ccdd9d2f3fe9a6e46
Angus M <angus@mm2.ca>
2023-08-19 11:53:13 -0700

    Trying to display the board

--- tictactoe/TicTacToe.js
+++ tictactoe/TicTacToe.js
@@ -1,4 +1,43 @@
-export default function Square() {
-    return <button className="square">X</button>;
+'use strict';
+
+const e = React.createElement;
+
+class tictactoe extends React.Component {
+  constructor(props) {
+    super(props);
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+    this.state = { liked: false };
   }
-  
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(tictactoe));
