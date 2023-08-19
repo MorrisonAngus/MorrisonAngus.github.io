ce3b1ed2b8c6e2c187cdb649bff24e78a5439b16
Angus M <angus@mm2.ca>
2023-08-19 11:46:51 -0700

    Updated the js file. Trying to see if this works

--- tictactoe/TicTacToe.js
+++ tictactoe/TicTacToe.js
@@ -1,26 +1,4 @@
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
+export default function Square() {
+    return <button className="square">X</button>;
   }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-const domContainer = document.querySelector('#React-game');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+  
