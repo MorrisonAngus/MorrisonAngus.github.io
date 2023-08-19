61a5bd2390ab2685fd441510f45e95fe0d67e902
Angus M <angus@mm2.ca>
2023-08-19 12:36:28 -0700

    trying to get board to show

--- tictactoe/TicTacToe.js
+++ tictactoe/TicTacToe.js
@@ -1,10 +1,20 @@
-export default function Square() {
-    return (
-    'button',
-    'square'
-        );
+'use strict';
+
+const board = React.createElement;
+
+class Square extends React.Component() {
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return board(
+                'button',
+                'square'
+            );
+    }
 }
 
 const domContainer = document.querySelector('#React-game');
 const root = ReactDOM.createRoot(domContainer);
-root.render(Square());
+root.render(board(Square));
