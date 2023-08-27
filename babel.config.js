9da8ef421df7e1f86e9816acf40d559713160ab0
Angus M <angus@mm2.ca>
2023-08-27 08:20:17 -0700

    Trying to get bable working correctly still

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env', // Required for transforming modern JavaScript features
+      '@babel/preset-react' // Required for transforming JSX syntax
+    ]
+  };
