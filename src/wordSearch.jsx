3d47dcb804b5d9f1940b17c90f5502aa52073b56
Angus M <angus@mm2.ca>
2023-08-28 10:56:25 -0700

    Updating word search

--- src/wordSearch.jsx
+++ src/wordSearch.jsx
@@ -8,24 +8,89 @@ class Board extends Component {
       super(props);
       this.state = {
         grid: [
-            ['W', 'E', 'L', 'A'],
-            ['O', 'A', 'I', 'T'],
-            ['R', 'R', 'N', 'T'],
-            ['D', 'U', 'C', 'K'],
-            ['S', 'H', 'S', 'G'],
-          ],
+          ['W', 'E', 'L', 'A'],
+          ['O', 'A', 'I', 'T'],
+          ['R', 'R', 'N', 'T'],
+          ['D', 'U', 'C', 'K'],
+          ['S', 'H', 'S', 'G'],
+        ],
+        clickedCells: new Set(),
+        isMouseDown: false,
+        startRowIndex: null,
+        startColIndex: null,
       };
     }
   
+    handleCellClick = (rowIndex, colIndex) => {
+      if (this.state.isMouseDown) {
+        this.handleCellSelection(rowIndex, colIndex);
+      } else {
+        this.setState({
+          clickedCells: new Set([`${rowIndex}-${colIndex}`]),
+          startRowIndex: rowIndex,
+          startColIndex: colIndex,
+        });
+      }
+    };
+  
+    handleCellSelection = (rowIndex, colIndex) => {
+      const { startRowIndex, startColIndex, grid } = this.state;
+      const newClickedCells = new Set();
+  
+      const rowStart = Math.min(startRowIndex, rowIndex);
+      const rowEnd = Math.max(startRowIndex, rowIndex);
+      const colStart = Math.min(startColIndex, colIndex);
+      const colEnd = Math.max(startColIndex, colIndex);
+  
+      for (let i = rowStart; i <= rowEnd; i++) {
+        for (let j = colStart; j <= colEnd; j++) {
+          newClickedCells.add(`${i}-${j}`);
+        }
+      }
+  
+      this.setState({
+        clickedCells: newClickedCells,
+      });
+    };
+  
+    handleMouseDown = (rowIndex, colIndex) => {
+      this.setState({
+        isMouseDown: true,
+        startRowIndex: rowIndex,
+        startColIndex: colIndex,
+      });
+    };
+  
+    handleMouseUp = () => {
+      this.setState({
+        isMouseDown: false,
+        startRowIndex: null,
+        startColIndex: null,
+      });
+    };
+  
     render() {
-      const { grid } = this.state;
+      const { grid, clickedCells } = this.state;
   
       return (
         <div className="board">
           {grid.map((row, rowIndex) => (
             <div key={rowIndex} className="row">
               {row.map((cell, colIndex) => (
-                <div key={colIndex} className="cell">
+                <div
+                  key={colIndex}
+                  className={`cell ${
+                    clickedCells.has(`${rowIndex}-${colIndex}`) ? 'clicked' : ''
+                  }`}
+                  onClick={() => this.handleCellClick(rowIndex, colIndex)}
+                  onMouseDown={() => this.handleMouseDown(rowIndex, colIndex)}
+                  onMouseEnter={() => {
+                    if (this.state.isMouseDown) {
+                      this.handleCellSelection(rowIndex, colIndex);
+                    }
+                  }}
+                  onMouseUp={this.handleMouseUp}
+                >
                   {cell}
                 </div>
               ))}
@@ -34,7 +99,7 @@ class Board extends Component {
         </div>
       );
     }
-}
+  }
 
 
 class Game extends Component {
