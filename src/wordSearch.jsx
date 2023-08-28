c3e4b2c232811306870ad40b25593f4b8c085606
Angus M <angus@mm2.ca>
2023-08-28 09:35:44 -0700

    Updating word search

--- src/wordSearch.jsx
+++ src/wordSearch.jsx
@@ -1,50 +1,36 @@
 'use strict';
 
-const { useState } = require("react");
+const { createElement, Component } = React;
 
-const Game = () => {
-    const [grid, setGrid] = useState([
-        ['W', 'O', 'R', 'D', 'S'],
-        ['E', 'A', 'R', 'C', 'H'],
-        ['L', 'I', 'N', 'E', 'S'],
-        ['A', 'T', 'T', 'F', 'G'],
-    ])
+class board extends Component {
 
-    const [selectedCells, setSelectedCells] = useState([]);
-    const wordsToFind = ['WORD', 'SEARCH', 'LINES', 'AT'];
+}
 
-    const handleCellClisk = (row, col) => {
-        const newSelectedCells =[...selectedCells, {row, col}];
-        setSelectedCells(newSelectedCells);
+
+class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            boardKey: 0, // Use this as a way to reset the board easily
+        };
     }
 
-    return (
-        <div>
-          <h1>Word Search Game</h1>
-          <div className="grid-container">
-            {grid.map((row, rowIndex) => (
-              <div key={rowIndex} className="row">
-                {row.map((cell, colIndex) => (
-                  <div
-                    key={colIndex}
-                    className={`cell ${selectedCells.some(cell => cell.row === rowIndex && cell.col === colIndex) ? 'selected' : ''}`}
-                    onClick={() => handleCellClick(rowIndex, colIndex)}
-                  >
-                    {cell}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-          <h2>Words to Find:</h2>
-          <ul>
-            {wordsToFind.map((word, index) => (
-              <li key={index}>{word}</li>
-            ))}
-          </ul>
-        </div>
-    );
-};
+    resetBoard() {
+        this.setState({ boardKey: this.state.boardKey + 1 });
+    }
+
+    render() {
+        const board = <Board key={this.state.boardKey} />;
+        const resetButton = <button onClick={() => this.resetBoard()}>Reset</button>;
+
+        return (
+            <div>
+                {board}
+                {resetButton}
+            </div>
+        );
+    }
+}
 
 
 
