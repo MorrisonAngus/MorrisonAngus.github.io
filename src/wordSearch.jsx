cab3731dffe085f169812599fce7891cf2905dbf
Angus M <angus@mm2.ca>
2023-08-28 11:27:22 -0700

    Updating word search

--- src/wordSearch.jsx
+++ src/wordSearch.jsx
@@ -10,7 +10,7 @@ class Board extends Component {
         grid: [
           ['W', 'E', 'L', 'A'],
           ['O', 'A', 'I', 'T'],
-          ['R', 'R', 'N', 'T'],
+          ['R', 'R', 'N', 'X'],
           ['D', 'U', 'C', 'K'],
           ['S', 'H', 'S', 'G'],
         ],
@@ -19,54 +19,73 @@ class Board extends Component {
         startRowIndex: null,
         startColIndex: null,
       };
+      this.wordList - ['WORDS', 'DUCK', 'SUN', 'AT', 'IT'];
     }
-  
-    handleCellClick = (rowIndex, colIndex) => {
-      if (this.state.isMouseDown) {
-        this.handleCellSelection(rowIndex, colIndex);
-      } else {
-        this.setState({
-          clickedCells: new Set([`${rowIndex}-${colIndex}`]),
-          startRowIndex: rowIndex,
-          startColIndex: colIndex,
-        });
-      }
-    };
-  
-    handleCellSelection = (rowIndex, colIndex) => {
-      const { startRowIndex, startColIndex, grid } = this.state;
-      const newClickedCells = new Set();
-  
-      const rowStart = Math.min(startRowIndex, rowIndex);
-      const rowEnd = Math.max(startRowIndex, rowIndex);
-      const colStart = Math.min(startColIndex, colIndex);
-      const colEnd = Math.max(startColIndex, colIndex);
-  
-      for (let i = rowStart; i <= rowEnd; i++) {
-        for (let j = colStart; j <= colEnd; j++) {
-          newClickedCells.add(`${i}-${j}`);
-        }
-      }
-  
-      this.setState({
-        clickedCells: newClickedCells,
-      });
-    };
-  
+
+    /* Handel the mouse functions for click and drag */
     handleMouseDown = (rowIndex, colIndex) => {
-      this.setState({
-        isMouseDown: true,
-        startRowIndex: rowIndex,
-        startColIndex: colIndex,
-      });
+        this.setState({
+            isMouseDown: true,
+            startRowIndex: rowIndex,
+            startColIndex: colIndex,
+        });
     };
-  
+
     handleMouseUp = () => {
-      this.setState({
-        isMouseDown: false,
-        startRowIndex: null,
-        startColIndex: null,
-      });
+        this.setState({
+          isMouseDown: false,
+          startRowIndex: null,
+          startColIndex: null,
+        });
+    };
+
+    /* Deal with what happens to cells when they are clicked*/
+    handleCellClick = (rowIndex, colIndex) => {
+        if(this.state.isMouseDown) {
+            this.handleCellSelection(rowIndex, colIndex);
+        } else {
+            this.setState({
+                clickedCells: new Set([`${rowIndex}-${colIndex}`]),
+                startRowIndex: rowIndex,
+                startColIndex: colIndex,
+            });
+        }
+    };
+
+    handleCellSelection = (rowIndex, colIndex) => {
+        const { startRowIndex, startColIndex, grid } = this.state;
+        const newClickedCells = new Set();
+    
+        const rowStart = Math.min(startRowIndex, rowIndex);
+        const rowEnd = Math.max(startRowIndex, rowIndex);
+        const colStart = Math.min(startColIndex, colIndex);
+        const colEnd = Math.max(startColIndex, colIndex);
+        
+        /* if the number of columns is greater then we only want to include those cells */
+        if(colEnd-colStart > rowEnd-rowStart){
+            for(let j = colStart; j<= colEnd; j++){
+                newClickedCells.add(`${i}-${j}`);
+            }
+        /* if the number of rows changed is greater than the number of columns changed add the row cells */
+        }else if(rowEnd-rowStart > colEnd-colStart){
+            for(let j = rowStart; j<= rowEnd; j++){
+                newClickedCells.add(`${i}-${j}`);
+            }
+        /* they are equal and we want to add on the diagnal */
+        }else{
+            for (let i = rowStart; i <= rowEnd; i++) {
+                for (let j = colStart; j <= colEnd; j++) {
+                    if(i===j){
+                        newClickedCells.add(`${i}-${j}`);
+                    }
+                }
+              }
+        }
+
+    
+        this.setState({
+          clickedCells: newClickedCells,
+        });
     };
   
     render() {
@@ -105,23 +124,14 @@ class Board extends Component {
 class Game extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            boardKey: 0, // Use this as a way to reset the board easily
-        };
-    }
-
-    resetBoard() {
-        this.setState({ boardKey: this.state.boardKey + 1 });
     }
 
     render() {
         const board = <Board key={this.state.boardKey} />;
-        const resetButton = <button onClick={() => this.resetBoard()}>Reset</button>;
 
         return (
             <div>
                 {board}
-                {resetButton}
             </div>
         );
     }
