4651685ffad8fe6d2ca61de9aeeec8d79b3d70a3
Angus M <angus@mm2.ca>
2023-09-02 14:33:21 -0700

    Fixing 'require not defined' error

--- src/thisDayInHistory.jsx
+++ src/thisDayInHistory.jsx
@@ -3,7 +3,7 @@
 const { createElement, Component } = React;
 
 // import { Configuration, OpenAIApi } from "openai";
-const {configuration, OpenAIApi} = "openai";
+const {configuration, OpenAIApi} = openai;
 configuration = new Configuration({
     organization: "org-2KEWsr9tVZglYFjJqfGTrOIf",
     apiKey: process.env.REACT_APP_GPT_API,
