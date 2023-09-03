ba4fd348aecd03ad318339bd3ae95aaed97a48ae
Angus M <angus@mm2.ca>
2023-09-03 10:03:16 -0700

    Updated this day in history.jsx to ES6 format

--- src/thisDayInHistory.jsx
+++ src/thisDayInHistory.jsx
@@ -1,59 +1,57 @@
 'use strict';
 
-const { createElement, Component } = React;
+import React, { Component } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
 
-import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: "org-2KEWsr9tVZglYFjJqfGTrOIf",
+    organization: 'org-2KEWsr9tVZglYFjJqfGTrOIf',
     apiKey: process.env.REACT_APP_GPT_API,
 });
+
 const openai = new OpenAIApi(configuration);
 
-
-// This displays a box with the info collected from chat gpt
 class Display extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: new Date(),// Get the date for the submission to chat gpt
-            output: 'This is where the output goes'
-        }
+            date: new Date(),
+            output: 'This is where the output goes',
+        };
         this.getDayDetails = this.getDayDetails.bind(this);
     }
 
     componentDidMount() {
-        // Call getDayDetails when the component mounts, this means that it only runs when initially loaded
         this.getDayDetails();
     }
 
     async getDayDetails() {
-        const prompt = "Say this is a test";
-    
+        const prompt = 'Say this is a test';
+
         try {
             const response = await openai.Completion.create({
-                engine: "text-davinci-002",
+                engine: 'text-davinci-002',
                 prompt: prompt,
                 max_tokens: 50,
                 n: 1,
                 stop: null,
                 temperature: 0.7,
             });
-    
+
             const generatedText = response.choices[0].text;
             this.setState({ output: generatedText });
         } catch (error) {
-            console.error("API Request Error:", error);
+            console.error('API Request Error:', error);
         }
     }
 
-    render () {
+    render() {
         return (
             <div className="GTP_output">
                 <h3>This day in history as presented by chat GPT.</h3>
                 <p>On this day the following happened:</p>
                 <p>{this.state.output}</p>
             </div>
-        )
+        );
     }
 }
 
