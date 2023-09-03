9b1de123705fc267189ae558eba15a4765f64e5f
Angus M <angus@mm2.ca>
2023-09-03 13:54:59 -0700

    Reintroduced webpack

--- src/thisDayInHistory.jsx
+++ src/thisDayInHistory.jsx
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     organization: 'org-2KEWsr9tVZglYFjJqfGTrOIf',
